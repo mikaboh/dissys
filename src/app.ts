95a3d229ef015b6b763967b93b7658c37c3ac52a
Mika Boehm <miboit03@hs-esslingen.de>
2022-11-24 20:09:42 +0100

    Remove unused routes

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
 import todosRouter from "./routes/todos";
 
 
@@ -16,8 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/todos', todosRouter);
 
 export default app;
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', (req: any, res: any, next: any) => {
-  res.render('index', { title: 'Express' });
-});
-
-export default router;
