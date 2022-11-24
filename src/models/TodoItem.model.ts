ad5d71b59b360b161e97666c479a22a24fd325e7
Mika Boehm <miboit03@hs-esslingen.de>
2022-11-24 18:01:19 +0100

    Add TodoItem model

---
+++ src/models/TodoItem.model.ts
@@ -0,0 +1,25 @@
+class TodoItem {
+    todo: string;
+    priority: number = 2;
+
+    constructor(todo: string, priority: number) {
+        this.todo = todo;
+        this.priority = priority;
+    }
+
+    getTodo() {
+        return this.todo;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+
+    setPriority(priority: number) {
+        this.priority = priority;
+    }
+
+    toString() {
+        return "TodoItem [todo=" + this.todo + ", priority=" + this.priority + "]";
+    }
+}
