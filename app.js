5e6b416d50cb311f2838de4a2bc25334f5770a87
Mika Boehm <miboit03@hs-esslingen.de>
2022-12-11 18:31:14 +0100

    Add swagger

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
 var todosRouter = require('./routes/todos');
 
@@ -13,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/todos', todosRouter);
 
 module.exports = app;
---
+++ swagger.js
@@ -0,0 +1,14 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'Distributed Systems - API',
+    },
+    host: 'localhost:3000',
+    schemes: ['http'],
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./app.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
